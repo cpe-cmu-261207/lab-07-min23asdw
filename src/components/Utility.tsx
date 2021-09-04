f93c05c78ab126d9034d428e53ec734ba938bca2
min23asdw <min23asdw@outlook.com>
2021-09-04 18:10:28 +0700

    Random clear

--- src/components/Utility.tsx
+++ src/components/Utility.tsx
@@ -1,8 +1,21 @@
+
+import { createCanvas, PixelPainterStore } from "../stores/PixelPainterStore"
+
+
 const Utility = () => {
+
+  const clear = () => {
+    PixelPainterStore.update(state => { state.canvas = createCanvas(false) })
+  }
+
+  const random = () => {
+    PixelPainterStore.update(state => { state.canvas = createCanvas(true) })
+  }
+
   return (
     <div className="flex justify-center space-x-3">
-      <button className="w-36">Clear</button>
-      <button className="w-36">Random color</button>
+      <button className="w-36" onClick={clear}>Clear</button>
+      <button className="w-36" onClick={random}>Random color</button>
     </div>
   )
 }
